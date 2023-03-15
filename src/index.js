c5896a75daf64e243d6f0031cf66c9d523dd059b
知之学吧 <121071795+zhizhixueba@users.noreply.github.com>
2023-03-15 22:53:32 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Meng
+ * Date: 2023-03
+ * Desc: 导出
+ */
+
+export { default as DataWidget } from './DataWidget'
+export { default as Datagram } from './Datagram'
+export { default as StateComponent } from './StateComponent'
+export { default as Store } from './Store'
