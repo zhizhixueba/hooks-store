57bd259aeee1b443b0902cc5aec0e1701c773857
meng <meng.liu1@bthome.com>
2023-05-12 11:06:01 +0800

    补充方法

--- src/LiveComponent.js
+++ src/LiveComponent.js
@@ -20,24 +20,32 @@ export default class LiveComponent extends React.Component {
   componentDidCatch(error, errorInfo) {
     if (this._liveData && this._liveData.onCatch) {
       this._liveData.onCatch(error, errorInfo)
+    }else {
+      super.componentDidCatch(error, errorInfo);
     }
   }
 
   componentDidMount() {
     if (this._liveData && this._liveData.onReady) {
       this._liveData.onReady();
+    }else {
+      super.componentDidMount();
     }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     if (this._liveData && this._liveData.getBeforeUpdate) {
       this._liveData.getBeforeUpdate(prevProps, prevState)
+    }else {
+      super.getSnapshotBeforeUpdate(prevProps, prevState);
     }
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     if (this._liveData && this._liveData.onUpdate) {
       this._liveData.onUpdate(prevProps, prevState, snapshot);
+    }else {
+      super.componentDidUpdate(prevProps, prevState, snapshot);
     }
   }
 
@@ -52,6 +60,8 @@ export default class LiveComponent extends React.Component {
   componentWillUnmount() {
     if (this._liveData && this._liveData.onDestroy) {
       this._liveData.onDestroy();
+    }else {
+      super.componentWillUnmount();
     }
   }
 
