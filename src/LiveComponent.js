a382ed5a1adbb24e770cddd3bf04b71d6a2852da
meng <meng.liu1@bthome.com>
2023-05-12 11:26:35 +0800

    删除调用

--- src/LiveComponent.js
+++ src/LiveComponent.js
@@ -20,48 +20,38 @@ export default class LiveComponent extends React.Component {
   componentDidCatch(error, errorInfo) {
     if (this._liveData && this._liveData.onCatch) {
       this._liveData.onCatch(error, errorInfo)
-    }else {
-      super.componentDidCatch(error, errorInfo);
     }
   }
 
   componentDidMount() {
     if (this._liveData && this._liveData.onReady) {
       this._liveData.onReady();
-    }else {
-      super.componentDidMount();
     }
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     if (this._liveData && this._liveData.getBeforeUpdate) {
       this._liveData.getBeforeUpdate(prevProps, prevState)
-    }else {
-      super.getSnapshotBeforeUpdate(prevProps, prevState);
     }
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     if (this._liveData && this._liveData.onUpdate) {
       this._liveData.onUpdate(prevProps, prevState, snapshot);
-    }else {
-      super.componentDidUpdate(prevProps, prevState, snapshot);
     }
   }
 
   shouldComponentUpdate(nextProps, nextState, nextContext) {
     if (this._liveData && this._liveData.shouldUpdate) {
       return this._liveData.shouldUpdate(nextProps, nextState, nextContext);
-    }else {
-      return super.shouldComponentUpdate(nextProps, nextState, nextContext);
+    } else {
+      return true;
     }
   }
 
   componentWillUnmount() {
     if (this._liveData && this._liveData.onDestroy) {
       this._liveData.onDestroy();
-    }else {
-      super.componentWillUnmount();
     }
   }
 
