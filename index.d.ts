6eda7984edea81be25c012fc2496bfe91e061749
meng <meng.liu1@bthome.com>
2023-06-09 07:36:03 +0800

    修改使用

--- index.d.ts
+++ index.d.ts
@@ -4,7 +4,7 @@
  * Desc: 导出
  */
 
-export { default as DataSource } from "./src/LiveData";
+export { default as LiveData } from "./src/LiveData";
 export { default as LiveModel } from "./src/LiveModel";
 export { default as HooksWidget } from "./src/HooksWidget";
 export { default as LiveComponent } from "./src/LiveComponent";
